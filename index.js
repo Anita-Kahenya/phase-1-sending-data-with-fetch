e7d343d6f40183c90c33f5a07a056c9b81f79107
Teresa <tenjerim@gmail.com>
2022-12-08 14:41:38 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData( name, email ){
+    return fetch("http://localhost:3000", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify
+        name,
+        email
+    })
+    .then("=>" (response){
+        return response.json()
+    })
+    .then("=>" (object){
+        document.body.innerHTML = Object["id"]
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+}
